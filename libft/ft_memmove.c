cb7a97bda0a1cb3f13f406a2e77a1fd4b6c32d04
Xintian <jasssmineeeeeee@gmail.com>
2026-07-23 01:18:16 +0800

    committing before i crash

---
+++ libft/ft_memmove.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chenx <chenx@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/07/23 00:57:51 by chenx             #+#    #+#             */
+/*   Updated: 2026/07/23 01:17:02 by chenx            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memmove(void *dest, const void *src, size_t len)
+{
+	size_t				i;
+	unsigned char		*d;
+	const unsigned char	*s;
+
+	if (!dest && !src)
+		return (NULL);
+	d = (unsigned char *)dest;
+	s = (const unsigned char *)src;
+	if (d < s)
+	{
+		i = 0;
+		while (i < len)
+		{
+			d[i] = s[i];
+			i++;
+		}
+	}
+	else
+		while (len--)
+			d[len] = s[len];
+	return (dest);
+}
