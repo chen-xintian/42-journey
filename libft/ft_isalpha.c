336189285b47a3921d0ac6483adefdb960592f57
Xintian <jasssmineeeeeee@gmail.com>
2026-07-24 13:37:37 +0800

    yay updated ver

--- libft/ft_isalpha.c
+++ libft/ft_isalpha.c
@@ -6,11 +6,11 @@
 /*   By: chenx <chenx@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/12 22:47:01 by chenx             #+#    #+#             */
-/*   Updated: 2026/07/21 14:29:09 by chenx            ###   ########.fr       */
+/*   Updated: 2026/07/24 13:35:11 by chenx            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h> 
+#include "libft.h"
 
 int	ft_isalpha(int c)
 {
@@ -20,16 +20,3 @@ int	ft_isalpha(int c)
 		return (0);
 }
 
-int	main(void)
-{
-	char	test;
-
-	test = '#';
-	write(1, "Testing ft_isalpha:\n", 20);
-	write(1, "Is test alpha? ", 15);
-	if (ft_isalpha(test))
-		write(1, "Yes\n", 4);
-	else
-		write(1, "No\n", 3);
-	return (0);
-}
--- libft/ft_isascii.c
+++ libft/ft_isascii.c
@@ -6,11 +6,11 @@
 /*   By: chenx <chenx@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/13 00:20:44 by chenx             #+#    #+#             */
-/*   Updated: 2026/07/21 14:29:13 by chenx            ###   ########.fr       */
+/*   Updated: 2026/07/24 13:35:14 by chenx            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
+#include "libft.h"
 
 int	ft_isascii(int c)
 {
@@ -18,18 +18,4 @@ int	ft_isascii(int c)
 		return (1);
 	else
 		return (0);
-}
-
-int	main(void)
-{
-	char	test;
-
-	test = '\n';
-	write(1, "Testing ft_isascii:\n", 20);
-	write(1, "Is test ascii? ", 15);
-	if (ft_isascii(test))
-		write(1, "Yes\n", 4);
-	else
-		write(1, "No\n", 3);
-	return (0);
 }
