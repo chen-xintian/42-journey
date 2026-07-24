c019cc7cfa84e3ebfb274f9a15a6d7d49f4557d1
Xintian <jasssmineeeeeee@gmail.com>
2026-07-24 18:16:18 +0800

    added ft_strlcpy.c

---
+++ libft/ft_strlcpy.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chenx <chenx@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/07/24 17:57:21 by chenx             #+#    #+#             */
+/*   Updated: 2026/07/24 18:12:04 by chenx            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcpy(char *dest, const char *src, size_t destsize)
+{
+	size_t	i;
+	size_t	src_len;
+
+	src_len = 0;
+	while (src[src_len])
+		src_len++;
+
+	if (destsize == 0)
+		return (src_len);
+
+	i = 0;
+	while (i < destsize - 1 && src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (src_len);
+}
