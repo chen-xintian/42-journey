1d3e42d686904d48c5c658afc385f2846a61ee22
Xintian <jasssmineeeeeee@gmail.com>
2026-07-28 14:42:27 +0800

    changed from ft_strlcpy to ft_memcpy

--- libft/ft_calloc.c
+++ libft/ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: chenx <chenx@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/27 14:25:14 by chenx             #+#    #+#             */
-/*   Updated: 2026/07/27 14:43:29 by chenx            ###   ########.fr       */
+/*   Updated: 2026/07/28 14:36:07 by chenx            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ void	*ft_calloc(size_t count, size_t size)
 {
 	void	*ptr;
 
+	if (size != 0 && count > SIZE_MAX / size)
+		return (NULL);
 	ptr = malloc(count * size);
 	if (!ptr)
 		return (NULL);
