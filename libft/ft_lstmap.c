94952c0293f44d4ef46059d33f6a19489adb7cf0
Xintian <jasssmineeeeeee@gmail.com>
2026-07-31 18:07:15 +0800

    added ft_lstmap.c

---
+++ libft/ft_lstmap.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstmap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chenx <chenx@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/07/31 18:00:01 by chenx             #+#    #+#             */
+/*   Updated: 2026/07/31 18:05:42 by chenx            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+#include "libft.h"
+
+t_list	*ft_lstmap(t_list *lst,
+					void *(*f)(void *),
+					void (*del)(void *))
+{
+	t_list	*new_list;
+	t_list	*new_node;
+
+	if (lst == NULL || f == NULL || del == NULL)
+		return (NULL);
+	new_list = NULL;
+	while (lst != NULL)
+	{
+		new_node = ft_lstnew(f(lst->content));
+		if (new_node == NULL)
+		{
+			ft_lstclear(&new_list, del);
+			return (NULL);
+		}
+		ft_lstadd_back(&new_list, new_node);
+		lst = lst->next;
+	}
+	return (new_list);
+}
